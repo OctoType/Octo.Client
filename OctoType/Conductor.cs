64dcd7fa636d4591b972d0f7762ff337c231cc97
pseudobear <2002michaelli@gmail.com>
2021-10-11 13:22:23 -0400

    add conductor class and timer utils

---
+++ OctoType/Conductor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OctoType.Utils;
+namespace OctoType {
+    public class Conductor {
+        private float songTime { set; get; }
+        private float startTime;
+        private float bpm { set; get; }
+        private float offset { set; get; }
+        private int barNum;
+
+        public Conductor(float bpm, float offset) {
+            this.barNum = 0;
+            this.songTime = 0;
+            this.bpm = bpm;
+            this.offset = offset;
+        }
+
+        public void startSong() {
+
+        }
+
+        // call this every frame
+        public void updateSongTime() {
+            startTime = TimeUtils.getTimer();
+        }
+    }
+}
---
+++ OctoType/Utils/TimeUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace OctoType.Utils {
+    
+    public static class TimeUtils {
+
+        public static float gameTime {
+            get {
+                return gameTime;
+            }
+            set {
+                gameTime = value;
+            }
+        }
+    }
+}
