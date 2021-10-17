70866d4a05a8e2ed9da36daa631a57c3cd1f5686
pseudobear <2002michaelli@gmail.com>
2021-10-17 17:09:57 -0400

    abstract input handler

---
+++ OctoType/Inputs/InputManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+
+namespace OctoType.Inputs {
+
+    public abstract class InputHandler {
+
+        protected static List<string> KeysHeld { set; get; }
+        protected static List<string> PressedKeys { set; get; }
+        protected static List<string> LiftedKeys { set; get; }
+        private static List<string> newKeysDown = new List<string>();
+
+        public void Update() {
+            newKeysDown.Clear();
+            PressedKeys.Clear();
+            LiftedKeys.Clear();
+            foreach(Keys key in Keyboard.GetState().GetPressedKeys()) {
+                string kval = key.ToString();
+                newKeysDown.Add(kval);
+                if(!KeysHeld.Contains(kval)) {
+                    PressedKeys.Add(kval);
+                    KeysHeld.Add(kval);          // technically it is held as it's pressed
+                }
+            }
+            foreach(string kval in KeysHeld) {
+                if(!newKeysDown.Contains(kval)) {
+                    LiftedKeys.Add(kval);
+                    KeysHeld.Remove(kval);       // remove keys that no longer are held
+                }
+            }
+            // here, we expect that KeysHeld is equal to newKeysDown
+            this.OnUpdate();
+        }
+
+        protected abstract void OnUpdate();
+    }
+}
