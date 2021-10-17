d4d7dcf7e9f52f343de4540462b09b06765e4075
pseudobear <2002michaelli@gmail.com>
2021-10-17 15:16:07 -0400

    implement song functinos

--- OctoType/Files/AudioManager.cs
+++ OctoType/Files/AudioManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using OctoType.Files;
+using OctoType.Utils;
 using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
 
 namespace OctoType.Audio {
 
@@ -15,32 +17,45 @@ namespace OctoType.Audio {
         /// Load a file into the audio manager. Because this class supports both songs and sound effects
         /// it is important to note this:
         /// - SONG files for beatmaps **MUST** contain song inside of their name, otherwise they will be loaded as sound effects
+        /// - Additionally, song file names must match the file name (recommended to be song.ogg)
         /// - sound effect files **MUST NOT** contain song, otherwise they will be loaded as songs
         /// </summary>
         public override void LoadFile(string name, string path) {
-            Uri uri = new Uri(path, UriKind.Relative);
-            Console.WriteLine("*LOG: Loading file for " + path + "*");
-            Song song = Song.FromUri(name, uri);
-            data.Add(name, song);
+            if(StringUtils.Normalize(name).Contains(SONG)) {
+                Uri uri = new Uri(path, UriKind.Relative);
+                Song song = Song.FromUri(name, uri);
+                data.Add(name, song);
+            } else {
+                SoundEffect soundEffect = SoundEffect.FromFile(path);
+                data.Add(name, soundEffect);
+            }
+            
         }
 
         /// <summary>
-        /// Plays a song with name. The 
+        /// Plays a song with name. The name MUST contain song 
         /// </summary>
-        public void playSong(string name) {
-            if(!name.Contains(SONG))
-            MediaPlayer.Play((Song) data[name]);
+        public void PlaySong(string name) {
+            if(StringUtils.Normalize(name).Contains(SONG))
+                MediaPlayer.Play((Song)data[name]);
+            else {
+                Console.WriteLine("ERR: line 31 AudioManager " + name + " is not a song.");
+            }
+        }
+        
+        public void PauseSong() {
+            MediaPlayer.Pause();
         }
 
-        public void pause() {
-
+        public void StopSong() {
+            MediaPlayer.Stop();
         }
 
-        public void stop() {
-
+        public void ResumeSong() {
+            MediaPlayer.Resume();
         }
 
-        public void resume() {
+        public void PlaySoundEffect(string name) {
 
         }
     }
--- OctoType/Game1.cs
+++ OctoType/Game1.cs
@@ -3,7 +3,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using OctoType.Utils;
 using OctoType.Screens;
-using OctoType.Audio;
 using System;
 
 namespace OctoType {
